4ad66eb78d3c81dc8fb0a6a31783197d3006316d
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2022-11-21 09:32:56 +0700

    Fix tailwindcss

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,7 +17,6 @@ export default function Index() {
       isLoading={ isLoading }>
       <div className="h-12 w-full bg-gradient-to-b from-blue-400 via-blue-800 to-blue-400 px-4 py-1 flex items-center justify-between">
         <div className="w-1/4">
-          {/* <img src="dist/logo.png" alt="Logo" class="inline-flex items-center h-8"> */}
           <div className="inline-flex items-center h-8">
             <Image
               src="logo.png"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+// const withMT = require("@material-tailwind/react/utils/withMT");
 
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
+module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{jsx,js,tsx,ts}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
