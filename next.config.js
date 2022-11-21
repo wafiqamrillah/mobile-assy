5a82926a5c14ffdf672502980f8566a6f31ceab8
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2022-11-21 09:04:40 +0700

    Initialize and change from Typescript to Javascript

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    legacyBrowsers: true
+  },
+  webpack: function (config) {
+    const originalEntry = config.entry;
+
+    config.entry = async () => {
+      const entries = await originalEntry();
+
+      if (
+        entries["main.js"] &&
+        !entries["main.js"].includes("./src/polyfills.js")
+      ) {
+        entries["main.js"].unshift("./src/polyfills.js");
+      }
+      return entries;
+    };
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
---
+++ src/hooks/useAuth.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import useSWR from "swr";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function useAuth({ middleware, redirectIfAuthenticated } = {}) {
+    const router = useRouter();
+
+    const { data, error, mutate } = useSWR(
+        `http://msi-ps.test/api/auth/get`,
+        async (...args) => axios
+            .get(args)
+            .then(res => res.data !== '' ? res.data : false)
+            .catch(error => {
+                throw error;
+            })
+    );
+
+    const login = async ({ setErrors, setStatus, ...props }) => {
+        setErrors([]);
+        setStatus(null);
+
+        return await axios
+            .post(`http://msi-ps.test/api/auth/login`, props)
+            .then(() => mutate());
+    }
+
+    const logout = async () => {
+        if (! error) {
+            await axios
+                .post(`http://msi-ps.test/api/auth/logout`)
+                .then(() => mutate());
+        }
+
+        router.replace('/login');
+        // window.location.pathname = '/login';
+    }
+
+    useEffect(() => {
+        if (middleware === 'guest' && redirectIfAuthenticated && data && data.constructor.name !== 'AxiosError') router.push(redirectIfAuthenticated);
+        if (middleware === 'auth' && ((!data && (typeof data !== 'undefined')) || error)) logout();
+    }, [data, error]);
+
+    return {
+        data,
+        error,
+        mutate,
+        login,
+        logout
+    };
+}
