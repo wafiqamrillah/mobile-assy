3485d310562a7499114a2ff8ba8f79923f946da0
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2022-12-06 17:23:45 +0700

    Add input pull list in index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,15 +13,16 @@ export default function Index() {
       isLoading={ isLoading }>
         <Navbar />
         
-        <div className="pt-16 flex-auto flex flex-col items-center justify-center">
+        <div className="pt-16 flex-auto flex flex-col items-center justify-center space-y-2">
           {
             ([
-              {href : '/adjust_qty', label: 'Adjust Qty'}
+              {href : '/adjust_qty', label: 'Adjust Qty'},
+              {href : '/input_pull_list', label: 'Input Pull List'},
             ]).map((item, index) => {
               return (
-                <Link key={ `button#${index}` } href={ item.href }>
+                <Link key={ `button#${index}` } href={ item.href } className="w-full">
                   <button
-                    className="p-6 bg-gradient-to-b from-blue-400 to-blue-800 hover:from-blue-100 hover:to-blue-400 border focus:from-blue-900 focus:to-blue-600 border-gray-500 rounded-lg focus:outline-none">
+                    className="p-6 w-full bg-gradient-to-b from-blue-400 to-blue-800 hover:from-blue-100 hover:to-blue-400 border focus:from-blue-900 focus:to-blue-600 border-gray-500 rounded-lg focus:outline-none">
                     { item.label }
                   </button>
                 </Link>
