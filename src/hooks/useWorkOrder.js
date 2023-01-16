26d523540a1ed3b84916b9b3cf469e77e33bd359
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2023-01-16 14:45:25 +0700

    Fix API URL and add ABAS Work Order Module on useWorkOrder

--- src/hooks/useWorkOrder.js
+++ src/hooks/useWorkOrder.js
@@ -1,9 +1,26 @@
 import axios from "axios";
+import { useRouter } from 'next/router';
 
 export default function useWorkOrder() {
+    const router = useRouter();
+
+    const findABASWorkOrder = async (number) => {
+        return await axios.get(
+            `${(router.basePath ?? 'http://192.168.0.235:8080/msi-ps-new').replace('mobile-assy', 'msi-ps-new')}/api/workorder/find_abas_work_order/${number}`
+        )
+        .then(res => res.data)
+        .catch(err => {
+            if (err.response?.status == 404) {
+                throw new Error("Data tidak ditemukan.");
+            } else {
+                throw err;
+            }
+        });
+    }
+
     const findFinishGoodWorkOrder = async (number) => {
         return await axios.get(
-            `http://192.168.0.114:8081/api/workorder/find_finish_good/${number}`
+            `${(router.basePath ?? 'http://192.168.0.235:8080/msi-ps-new').replace('mobile-assy', 'msi-ps-new')}/api/workorder/find_finish_good/${number}`
         )
         .then(res => res.data)
         .catch(err => {
@@ -17,7 +34,7 @@ export default function useWorkOrder() {
 
     const findSemiFinishGoodWorkOrder = async (number) => {
         return await axios.get(
-            `http://192.168.0.114:8081/api/workorder/find_semi_finish_good/${number}`
+            `${(router.basePath ?? 'http://192.168.0.235:8080/msi-ps-new').replace('mobile-assy', 'msi-ps-new')}/api/workorder/find_semi_finish_good/${number}`
         )
         .then(res => res.data)
         .catch(err => {
@@ -46,7 +63,7 @@ export default function useWorkOrder() {
             throw new Error('Tidak ada data part semi finish good yang diperiksa');
         }
 
-        const partFinishGood = await axios.get(`http://192.168.0.114:8081/api/part/find_by_idabas/${workOrderFG.idabas}`)
+        const partFinishGood = await axios.get(`${(router.basePath ?? 'http://192.168.0.235:8080/msi-ps-new').replace('mobile-assy', 'msi-ps-new')}/api/part/find_by_idabas/${workOrderFG.idabas}`)
             .then(res => res.data)
             .catch(err => {
                 if (err.response?.status == 404) {
@@ -62,7 +79,7 @@ export default function useWorkOrder() {
     const getSFGOutstandingLabels = async (workOrderSFGNumber) => {
         return await axios
             .get(
-                `http://192.168.0.114:8081/api/datalabel/get_sfg_outstanding_label_by_wo/${workOrderSFGNumber}`
+                `${(router.basePath ?? 'http://192.168.0.235:8080/msi-ps-new').replace('mobile-assy', 'msi-ps-new')}/api/datalabel/get_sfg_outstanding_label_by_wo/${workOrderSFGNumber}`
             )
             .then(res => res.data)
             .catch(err => {
@@ -80,7 +97,7 @@ export default function useWorkOrder() {
 
         return await axios
             .post(
-                `http://192.168.0.114:8081/api/workorder/adjust_qty`, form
+                `${(router.basePath ?? 'http://192.168.0.235:8080/msi-ps-new').replace('mobile-assy', 'msi-ps-new')}/api/workorder/adjust_qty`, form
             )
             .then(res => {
                 const response = res.data;
@@ -98,7 +115,7 @@ export default function useWorkOrder() {
     const getAllNGClassifications = async() => {
         return await axios
             .get(
-                `http://192.168.0.114:8081/api/ngclassifications/get_all`
+                `${(router.basePath ?? 'http://192.168.0.235:8080/msi-ps-new').replace('mobile-assy', 'msi-ps-new')}/api/ngclassifications/get_all`
             )
             .then(res => res.data)
             .catch(err => {
@@ -113,7 +130,7 @@ export default function useWorkOrder() {
     const getPullListsByWorkOrderNumber = async(workOrderNumber) => {
         return await axios
             .get(
-                `http://192.168.0.114:8081/api/pulllist/get_pull_lists_by_wo/${workOrderNumber}`
+                `${(router.basePath ?? 'http://192.168.0.235:8080/msi-ps-new').replace('mobile-assy', 'msi-ps-new')}/api/pulllist/get_pull_lists_by_wo/${workOrderNumber}`
             )
             .then(res => res.data)
             .catch(err => {
@@ -130,7 +147,7 @@ export default function useWorkOrder() {
 
         return await axios
             .post(
-                `http://192.168.0.114:8081/api/pulllist/store`, form
+                `${(router.basePath ?? 'http://192.168.0.235:8080/msi-ps-new').replace('mobile-assy', 'msi-ps-new')}/api/pulllist/store`, form
             )
             .then(res => {
                 const response = res.data;
@@ -145,7 +162,91 @@ export default function useWorkOrder() {
             });
     }
     
+    const submitAbasPullList = async (workOrder, form) => {
+        form.work_order_number = workOrder.number;
+
+        // If there is no work order number, then show error
+        if (!form.work_order_number) throw new Error("Data input tidak valid. Tidak ada nomor work order.");
+        // If there is no production date and time inputted, then show error
+        if (!form.production_date || !form.production_time) throw new Error("Data input tidak valid. Tidak ada tanggal atau waktu produksi yang dimasukkan.");
+
+        let success_responses = [];
+        let error_responses = [];
+
+        let good_qty = 0, ng_qty = 0, ng_classifications = [];
+        good_qty = form.good_qty ?? good_qty;
+        ng_qty = form.ng_qty ?? ng_qty;
+        ng_classifications = form.ng_classifications ?? ng_classifications;
+
+        // If there is no quantity inputted, then show error
+        if (good_qty + ng_qty <= 0) throw new Error("Data input tidak valid. Tidak ada quantity yang dimasukkan.");
+
+        // If there is NG quantity, but there is no NG Classification inputted, then show error
+        if (ng_qty > 0 && ng_classifications.length <= 0) throw new Error("Data input tidak valid. Klasifikasi NG tidak dimasukkan.");
+
+        let requests = [];
+
+        if (ng_qty > 0 && ng_classifications.length > 0) {
+            ng_classifications.forEach(function(classification) {
+                requests.push({
+                    'wo_number' : form.work_order.number,
+                    'wo_number_completion' : `${form.work_order.number}001`,
+                    'work_order' : form.work_order,
+                    'part' : form.work_order.part,
+                    'production_date' : form.production_date,
+                    'production_time' : form.production_time,
+                    'good_qty' : 0,
+                    'ng_qty' : classification.qty,
+                    'reason' : classification.ng_code,
+                });
+            });
+        }
+
+        if (good_qty > 0) {
+            requests.push({
+                'wo_number' : form.work_order.number,
+                'wo_number_completion' : `${form.work_order.number}001`,
+                'work_order' : form.work_order,
+                'part' : form.work_order.part,
+                'production_date' : form.production_date,
+                'production_time' : form.production_time,
+                'good_qty' : good_qty,
+                'ng_qty' : 0,
+                'reason' : null,
+            });
+        }
+        
+        // If there is no request generated, then show error
+        if (requests.length <= 0) throw new Error("Data tidak valid. Tidak ada data yang diproses ke ABAS.");
+
+        for (const request of requests) {
+            try {
+                const response = await axios
+                    .post(
+                        `${(router.basePath ?? 'http://192.168.0.235:8080/msi-ps-new').replace('mobile-assy', 'msi-ps-new')}/api/pulllist/abas_store`, request
+                    )
+                    .then(res => res.data)
+                    .catch(err => {
+                        throw new Error(err.response?.statusText ?? err.message);
+                    });
+
+                if (!response.status) throw new Error('There is no response data status from server.');
+                if (response.status !== 'success') throw new Error(response.message ?? 'Proses input gagal'); 
+
+                success_responses.push(response);
+            } catch (error) {
+                error_responses.push(error.message);
+            }
+        }
+
+        return {
+            success : success_responses,
+            error : error_responses
+        };
+    }
+    
     return {
+        findABASWorkOrder,
         findFinishGoodWorkOrder,
         findSemiFinishGoodWorkOrder,
         checkCompabilityWorkOrder,
@@ -153,6 +254,7 @@ export default function useWorkOrder() {
         adjustQuantity,
         getAllNGClassifications,
         getPullListsByWorkOrderNumber,
-        submitPullList
+        submitPullList,
+        submitAbasPullList
     };
 }
